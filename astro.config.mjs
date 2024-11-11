8672f2d54d72bbc9dc7dc56746e3f9a7b7c8ac3a
Williams <williams@Willies.local>
2024-11-11 06:08:34 +0100

    complete tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   site: "https://williamsohworuka.github.io",
   base: "/astro-blog",
+  integrations: [react()],
 });
---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  console.log("JODD");
+  document.querySelector(".nav-links").classList.toggle("expanded");
+});
